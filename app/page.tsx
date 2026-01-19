51beb20e8d983a0a667ea82ac22ad9f755ab463d
Nicolas Teixeira <nicolas.teixeira.ext@siemens-energy.com>
2026-01-18 23:10:36 -0300

    fix coderabbit suggestion

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <main className="main-container">
+      <h1 className="text-2xl font-semibold">CoinPulse</h1>
+    </main>
+  );
 };
 
 export default Page;
